7c1d46f9321ae2f1e148876c9f178e8323091633
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-15 06:07:50 +0600

    Added font style

---
+++ src/util/fonts/fontStyles.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+const FONTS = StyleSheet.create({
+  // ################# Bold ##################
+  bold46: {
+    color: 'white',
+    fontSize: 46,
+  },
+  bold11: {
+    color: 'white',
+    fontSize: 11,
+  },
+  bold10: {
+    color: 'white',
+    fontSize: 10,
+  },
+
+  // ################# Regular  ##################
+  regular26: {
+    color: 'white',
+    fontSize: 26,
+  },
+  regular8: {
+    color: 'white',
+    fontSize: 8,
+  },
+
+  // ################# Medium ##################
+  medium16: {
+    color: 'white',
+    fontSize: 16,
+  },
+  medium10: {
+    color: 'white',
+    fontSize: 10,
+  },
+});
+
+export default FONTS;
