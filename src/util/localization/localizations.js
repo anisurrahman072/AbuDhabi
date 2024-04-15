567923a59210a8f37b72841c6756f0087e3b1c47
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-15 06:12:49 +0600

    Updated localization struct

---
+++ src/util/localization/localizations.js
@@ -0,0 +1,12 @@
+// "en" means english
+// "ar" means arabic
+export default LOCALIZATIONS = {
+  loginScreen: {
+    en: {
+      title: 'Welcome to the new intranet',
+    },
+    ar: {
+      title: 'مرحباً! الى الشبكة الداخلية​',
+    },
+  },
+};
